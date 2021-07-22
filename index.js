5446c1602b6ed0e85726078e464a95e69cb4f95b
Angel Guerrero <aguerrero@aguerrero-01120-mac.local>
2021-07-22 11:41:16 -0500

    highlighted the campus

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function initMap() {
   const buttons = [
     ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
     ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
-    ["Tilt Down", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
-    ["Tilt Up", "tilt", 20, google.maps.ControlPosition.TOP_CENTER],
+    ["Tilt Down", "tilt", 20, google.maps.ControlPosition.BOTTOM_CENTER],
+    ["Tilt Up", "tilt", -20, google.maps.ControlPosition.TOP_CENTER],
   ];
   buttons.forEach(([text, mode, amount, position]) => {
     const controlDiv = document.createElement("div");
@@ -99,6 +99,19 @@ function initMap() {
       infoWindow.open(marker.getMap(), marker);
     });
   };
+
+  var coordinates = buildCoordinatesArrayFromString(kmlData);
+  const HarvardPolygon = new google.maps.Polygon({
+    paths: coordinates,
+    strokeColor: '#FF3158',
+    strokeOpacity: 1,
+    strokeWeight: 3,
+    fillColor: '#FF3158',
+    fillOpacity: 0.3
+  });
+
+  HarvardPolygon.setMap(map);
+
 };
 
 // Put all marker metadata here
@@ -142,8 +155,68 @@ const meta = [
     sid: 'GycExKiYVFp',
     description: 'Harvard has finished an eight-month renovation of Robinson Hall, the building housing the History Department, according to Associate Dean for Physical Resources and Planning Michael N. Lichten.'
   },
-  {
-    position: {lat: 42.373431927523875, lng: -71.1219333484599},
-    title: 'Harvard Yearbook'
-  }
 ];
+
+function buildCoordinatesArrayFromString(MultiGeometryCoordinates){
+  var finalData = [];
+  var grouped = MultiGeometryCoordinates.split("\n");
+
+  grouped.forEach(function(item, i){
+      let a = item.trim().split(',');
+
+      finalData.push({
+          lng: parseFloat(a[0]),
+          lat: parseFloat(a[1])
+      });
+  });
+
+  return finalData;
+}
+
+let kmlData = `-71.1220282,42.3764639
+-71.1222206, 42.3765968
+-71.1235832,42.3762798
+-71.1242054,42.3760975
+-71.1244951,42.375939
+-71.1247955,42.3750909
+-71.1234598,42.3747382
+-71.1231486,42.3744608
+-71.1241572,42.3738069
+-71.1229566,42.3733452
+-71.1233107,42.3728537
+-71.1243997,42.371962
+-71.1218033,42.3706463
+-71.1224256,42.3698536
+-71.1214814,42.3697743
+-71.1193357,42.3698377
+-71.118134,42.3696951
+-71.1172757,42.3692036
+-71.1162672,42.3677768
+-71.1146901,42.3677451
+-71.1146364,42.3693304
+-71.1134348,42.3692432
+-71.1133168,42.3700121
+-71.1161703,42.3709632
+-71.1155266,42.3715022
+-71.1155695,42.372168
+-71.1147112,42.3714705
+-71.114282,42.3719778
+-71.1134666,42.3718509
+-71.1123508,42.3737532
+-71.1133808,42.3741337
+-71.1126941,42.3752116
+-71.1124366,42.3768602
+-71.1115354,42.3771138
+-71.1123508,42.37965
+-71.1113638,42.37965
+-71.1111921,42.3807595
+-71.1120933,42.3814886
+-71.1141318,42.3803474
+-71.1144966,42.3807595
+-71.1163419,42.3808229
+-71.1159986,42.3836125
+-71.1194318,42.3838661
+-71.1195606,42.3825981
+-71.1201185,42.3776686
+-71.1191958,42.3751482
+-71.1220282,42.3764639`
